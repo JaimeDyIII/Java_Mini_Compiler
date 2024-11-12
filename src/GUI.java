41c280b21949e6e96fc794537c7680ef21f4ff00
jdy <dyiiijaime@gmail.com>
2024-11-12 18:12:20 +0800

    added clear file and button disabling if there is no file

--- src/GUI.java
+++ src/GUI.java
@@ -27,7 +27,7 @@ public class GUI extends JFrame {
         JTextArea codeTextArea = new JTextArea();
         codeTextField.setEditable(false);
         codeTextArea.setEditable(false);
-
+        
         codeTextArea.setBorder(new CompoundBorder(
             new LineBorder(Color.GRAY, 1),
             new EmptyBorder(10, 10, 10, 10)
@@ -35,29 +35,6 @@ public class GUI extends JFrame {
         
         codeTextField.setPreferredSize(new Dimension(400, 30));
 
-        JButton openFileButton = new JButton("Open File");
-        openFileButton.addActionListener((ActionEvent e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("java", "txt"));
-            fileChooser.setDialogTitle("Select File to Open");
-            
-            int result = fileChooser.showOpenDialog(GUI.this);
-            
-            if (result == JFileChooser.APPROVE_OPTION) {
-                file = fileChooser.getSelectedFile();
-                
-                try {
-                    String codeText = new String(Files.readAllBytes(file.toPath()));
-                    codeTextArea.setText(codeText);
-                    codeTextField.setText("File Selected!");
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-
-            } else {
-                codeTextField.setText("No file selected.");
-            }
-        });
 
         JButton lexicalAnalysisButton = new JButton("Lexical Analysis");
         lexicalAnalysisButton.addActionListener((ActionEvent e) -> {
@@ -85,7 +62,48 @@ public class GUI extends JFrame {
 
         JButton clearButton = new JButton("Clear");
         clearButton.addActionListener((ActionEvent e) -> {
+            file = null;
+            codeTextField.setText("Cleared file!");
+            codeTextArea.setText(null);
+            lexicalAnalysisButton.setEnabled(false);
+            syntaxAnalysisButton.setEnabled(false);
+            semanticAnalysisButton.setEnabled(false);
         });
+
+        JButton openFileButton = new JButton("Open File");
+        openFileButton.addActionListener((ActionEvent e) -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("java", "txt"));
+            fileChooser.setDialogTitle("Select File to Open");
+            
+            int result = fileChooser.showOpenDialog(GUI.this);
+            
+            if (result == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+                
+                try {
+                    if(file != null){
+                        lexicalAnalysisButton.setEnabled(true);
+                        syntaxAnalysisButton.setEnabled(true);
+                        semanticAnalysisButton.setEnabled(true);
+                    }
+                    String codeText = new String(Files.readAllBytes(file.toPath()));
+                    codeTextArea.setText(codeText);
+                    codeTextField.setText("File Selected!");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            } else {
+                codeTextField.setText("No file selected.");
+            }
+        });
+
+        if(file == null){
+            lexicalAnalysisButton.setEnabled(false);
+            syntaxAnalysisButton.setEnabled(false);
+            semanticAnalysisButton.setEnabled(false);
+        }
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(5, 1));
